d718a65139347d608157521c9def76881dcf804c
panchajanyaray <110964719+panchajanyaray@users.noreply.github.com>
2022-08-10 14:31:29 +0530

    Add files via upload

---
+++ solarsystem.py
@@ -0,0 +1,18 @@
+class planet:
+    def input1(self):
+        self.name=input("Enter name:")
+        self.surfaceglasses=int(input("Enter surfaceglasses:"))
+        self.moons=int(input("Enter the no of moons:"))
+        self.rings=input("Enter rings:")
+    def count_moon(self):
+        L=[]
+        print("Enter ",n,"elements:")
+        for i in range(n):
+            k=int(input())
+            L.append(k);
+        for i in range(len(L)):
+            c=0
+            for j in range(1,L[i]+1):
+                if rings==yes:
+                    c=c+1
+print("List is ",L)
